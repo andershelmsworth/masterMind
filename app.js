87d89d15aa75626be3ba4110d195d1d63010e94a
andershelmsworth <andy.helmsworth@gmail.com>
2020-11-29 15:51:32 -0800

    UX improvements

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(function (err, req, res, next) {
  * Launch communication
  ******************/
 
-const port = 6725;
+const port = 6727;
 app.listen(port);
 console.log('Server is running at http://localhost:8080/.\nCMD+C to quit.');
 
